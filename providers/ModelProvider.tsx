43c3fc1e32f36008b9b30640ff7605b05f51a68b
Madros Madros <yasernniihed@gmail.com>
2023-11-28 23:09:44 +0300

    fix moon

--- providers/ModelProvider.tsx
+++ providers/ModelProvider.tsx
@@ -10,7 +10,7 @@ interface ModelProviderProps {
     products: ProductWithPrice[];
 }
 
-const ModelProvider: React.FC<ProductWithPrice> = ({
+const ModelProvider: React.FC<ModelProviderProps> = ({
     products
 }) => {
     
