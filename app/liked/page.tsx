7f373fe95d5dae3dcd9997aa3e730630fa41626c
Madros Madros <yasernniihed@gmail.com>
2023-11-30 18:31:07 +0300

    g

--- app/liked/page.tsx
+++ app/liked/page.tsx
@@ -16,42 +16,41 @@ const Liked = async () => {
         bg-neutral-900 
         rounded-lg 
         h-full 
-        w-full 
+       w-full 
         overflow-hidden 
         overflow-y-auto
       "
     >
       <Header>
         <div className="mt-20">
-          <div 
+         <div 
             className="
-              flex 
+             flex 
               flex-col 
               md:flex-row 
-              items-center 
               gap-x-5
             "
           >
-            <div className="relative h-32 w-32 lg:h-44 lg:w-44">
+        <div className="relative h-32 w-32 lg:h-44 lg:w-44">
               <Image
                 className="object-cover"
                 fill
-                src="/images/liked.png"
+               src="/images/liked.png"
                 alt="Playlist"
-              />
+            />
             </div>
             <div className="flex flex-col gap-y-2 mt-4 md:mt-0">
               <p className="hidden md:block font-semibold text-sm">
                 Playlist
               </p>
               <h1 
-                className="
+             className="
                   text-white 
                   text-4xl 
                   sm:text-5xl 
-                  lg:text-7xl 
+                lg:text-7xl 
                   font-bold
-                "
+              "
               >
                 Liked Songs
               </h1>
